8d3303b4f4713fb3f3501b27ddc49e052f9d2e3b
Samael Pereira Simoes <samael.simoes@gmail.com>
2018-02-18 17:14:58 -0300

    WebPack react intro

---
+++ src/main/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import 'modules/bootstrap/dist/css/bootstrap.css'
+import 'modules/font-awesome/css/font-awesome.css'
+
+
+export default props => (
+	<div className='container'>
+		<h1> HELLOW WORLD </h1>
+	</div>
+)
